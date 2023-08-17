a0c4d2a99fa3e3e1bd2ee77e818bdba044377c4a
kyaw-min-khant <kyawminkhant8237@gmail.com>
2023-08-17 19:52:14 +0630

    imageUploads

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((data) => console.log("DB Collection Success"))
   .catch((err) => console.log(err));
-app.use(express.static(path.join(__dirname)));
+app.use("uploads", express.static(path.join(__dirname)));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOption));
 app.use(bodyParser.json());
